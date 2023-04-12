13c4622b3480549c72f3b5148caea6857bac6b35
lorionj10 <armandoxd_f.o.b@hotmail.com>
2023-04-11 18:27:24 -0700

    first commit

---
+++ src/app/auth/pages/login/login.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import Swal from 'sweetalert2';
+
+import { AuthService } from '../../services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: []
+})
+export class LoginComponent {
+
+  miFormulario: FormGroup = this.fb.group({
+    email:    ['', [ Validators.required, Validators.email ]],
+    password: ['', [ Validators.required, Validators.minLength(6) ]]
+  });
+
+  constructor( private fb: FormBuilder,
+               private router: Router,
+               private authService: AuthService ){  }
+
+  login() {
+    
+    const { email, password } = this.miFormulario.value;
+
+    this.authService.login( email, password )
+      .subscribe( ok => {
+        //console.log(resp);
+        if ( ok === true ) {
+          this.router.navigateByUrl('/dashboard');
+        } else {
+          Swal.fire('Error', ok, 'error');  // Swal.fire('Error', ok, Error);
+        }
+      });
+
+    }
+
+}
